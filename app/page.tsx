97c3830e402cec500ebcd72ca50c6f7b8a79d0b3
Derrick Strong <derrickmstrong@gmail.com>
2024-01-27 09:54:19 -0600

    Style project pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,29 +7,31 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto py-10">
-      <h1 className="text-7xl font-extrabold">
+      <h1 className="ml-2 md:ml-0 text-7xl font-extrabold">
         Ahola, I&apos;m{" "}
         <span className="bg-gradient-to-r from-cyan-400 to-blue-600 bg-clip-text text-transparent">
           Derrick
         </span>
         !
       </h1>
-      <p className="my-3 text-xl text-gray-600">
+      <p className="my-3 ml-2 md:ml-0 text-xl text-gray-600">
         Check out my latest adventures...
       </p>
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Adventures</h2>
+      <h2 className="mt-24 ml-2 md:ml-0 font-bold text-gray-700 text-3xl">
+        My Adventures
+      </h2>
       <div className="mt-2 grid grid-cols-1 md:grid-cols2 lg:grid-cols-3 gap-8 py-3">
         {projects.map((project) => (
           <Link
             href={`/projects/${project.slug}`}
             key={project._id}
-            className="border-2 border-gray-500 rounded-lg my-2 p-1 hover:scale-105 hover:border-blue-500 transition"
+            className="border-2 border-gray-500 rounded-lg m-2 md:m-0 p-1 hover:scale-105 hover:border-blue-500 transition"
           >
             {project.image && (
               <Image
                 src={project.image}
                 alt=""
-                width={750}
+                width={6000}
                 height={300}
                 className="object-cover rounded-lg border border-gray-500"
               />
--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -11,6 +11,21 @@ export default async function Project({ params } : ProjectProps) {
     const project = await getProject(slug);
 
     return (
-        <div>{project.name}</div>
-    )
+      <div className="max-w-3xl mx-auto py-20">
+        <header className="flex items-center justify-between">
+          <h1 className="bg-gradient-to-r from-cyan-400 to-blue-600 bg-clip-text text-transparent ml-2 md:ml-0 py-2 text-5xl drop-shadow font-extrabold">
+            {project.name}
+          </h1>
+          <a
+            href={project.url}
+            title="View Project"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-gray-100 rounded-lg text-gray-500 px-4 py-2 mr-2 md:mr-0 font-bold whitespace-nowrap hover:bg-gray-200 hover:text-gray-400 transition"
+          >
+            View Project
+          </a>
+        </header>
+      </div>
+    );
 }
